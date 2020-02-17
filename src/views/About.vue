87eb011a429ffac872fc7e87d088c9b2b450ccfb
wangwh01 <wangwh01@fenbi.com>
2020-02-17 17:37:20 +0800

    MOD: 生成海报分两步

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,48 +1,83 @@
 <template>
-    <div class="about">
-        <img style="width: 200px; height: 360px" v-if="dataURL" :src="dataURL"/>
-        <div v-else ref="courseInfo" class="img">
-            <img style="width: 10rem; height: 10rem;" src="../assets/favicon.png"/>
-            <span class="text">
-                这是我的头像
-            </span>
-            <p class="scan">长按识别二维码</p>
-            <qrcode class="qr_code" value="www.baidu.com" :options="{ size: 65 }"></qrcode>
+    <div>
+        <div v-show="!startTransfer" class="about">
+            <div class="input-item">
+
+                <group>
+                    <x-input style="font-size: 15px" title="海报标题" v-model="title" placeholder="请输入海报标题"></x-input>
+                    <x-input style="font-size: 15px" title="海报文案" v-model="content" placeholder="请输入海报标题"></x-input>
+                    <x-input style="font-size: 15px" title="海报标题" v-model="description" placeholder="请输入海报标题"></x-input>
+                    <x-input style="font-size: 15px" title="海报标题" v-model="nfrom" placeholder="请输入海报标题"></x-input>
+                </group>
+            </div>
+            <x-button :gradients="['#1d88eb', '#1d88eb']" @click.native="toImage()">生成海报</x-button>
         </div>
-        <van-button type="info" size="small" @click="toImage()">{{ dataURL ? '返回' : '生成海报'}}</van-button>
+
+        <div v-if="startTransfer">
+            <div v-if="dataURL">
+                <img style="width: 100%" :src="dataURL"/>
+                <div style="padding: 10px">
+                    <x-button :gradients="['#1d88eb', '#1d88eb']" @click.native="toImage()">返回重新生成</x-button>
+                </div>
+            </div>
+            <div v-show="!dataURL">
+                <div id="capture" class="img" style="width: 100%; height: 100%">
+                    <img style="width: 100%;" :onload="imgOnload()" src="../assets/WechatIMG42.jpeg"/>
+                    <qrcode class="qr-code" size="79" value="https://vux.li?x-page=demo_qrcode"></qrcode>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     // @ is an alias to /src
-    import Vue from 'vue';
-    import {Button} from 'vant';
     import html2canvas from 'html2canvas';
-    import QRcode from '@xkeshi/vue-qrcode';
-
-    Vue.use(Button);
+    import {XButton} from 'vux';
+    import {XInput} from 'vux';
+    import {Qrcode} from 'vux';
 
     export default {
         name: 'About',
         components: {
-            qrcode : QRcode
+            XButton,
+            XInput,
+            Qrcode
         },
         data() {
             return {
-                dataURL: ''
+                dataURL: '',
+                title: '',
+                content: '',
+                description: '',
+                nfrom: '',
+                startTransfer: false,
+                load: false
             };
         },
+        watch: {
+            load(newV) {
+                if (newV) {
+                    html2canvas(document.getElementById('capture')).then((canvas) => {
+                        const dataURL = canvas.toDataURL('assets/png', 1.0);
+                        this.dataURL = dataURL;
+                    });
+                }
+            }
+        },
         methods: {
+            imgOnload() {
+                setTimeout(() => {
+                    this.load = true;
+                }, 1000);
+            },
             toImage() {
                 if (this.dataURL) {
                     this.dataURL = '';
+                    this.startTransfer = false;
+                    this.load = false;
                 } else {
-                    html2canvas(this.$refs.courseInfo, {
-                        backgroundColor: null
-                    }).then((canvas) => {
-                        const dataURL = canvas.toDataURL('assets/png');
-                        this.dataURL = dataURL;
-                    });
+                    this.startTransfer = true;
                 }
             }
         }
@@ -50,22 +85,39 @@
 </script>
 
 <style lang="scss">
+
+    span {
+        width: 100px;
+    }
+
     .about {
+        margin-top: 30px;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
         padding: 20px;
 
-        .img {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            padding: 20px;
+        .input-item {
+            width: 100%;
+            padding: 10px 0;
+        }
+    }
 
-            .text {
-                margin-top: 20px;
+    .img {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        .qr-code {
+            position: absolute;
+            left: 44px;
+            top: 1145px;
+
+            .content {
+                width: 150px;
+                height: 150px
             }
         }
     }
---
+++ vue.config.js
@@ -0,0 +1,46 @@
+const autoprefixer = require('autoprefixer');
+const pxtorem = require('postcss-pxtorem');
+const merge = require('webpack-merge');
+const tsImportPluginFactory = require('ts-import-plugin');
+
+module.exports = {
+    parallel: false,
+    outputDir: 'dist',
+    publicPath: process.env.NODE_ENV === 'production' ? '/vant-demo/' : '/',
+    chainWebpack: config => {
+        config.module
+            .rule('ts')
+            .use('ts-loader')
+            .tap(options => {
+                options = merge(options, {
+                    transpileOnly: true,
+                    getCustomTransformers: () => ({
+                        before: [
+                            tsImportPluginFactory({
+                                libraryName: 'vant',
+                                libraryDirectory: 'es',
+                                style: true
+                            })
+                        ]
+                    }),
+                    compilerOptions: {
+                        module: 'es2015'
+                    }
+                });
+                return options;
+            });
+    },
+    css: {
+        loaderOptions: {
+            postcss: {
+                plugins: [
+                    autoprefixer(),
+                    pxtorem({
+                        rootValue: 75,
+                        propList: ['*']
+                    })
+                ]
+            }
+        }
+    }
+};
